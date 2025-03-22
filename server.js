e5915f2fc879b4190f3fe41c1719e1a91e8e945b
Rishi Gupta <rishigupta.offical@gmail.com>
2025-03-22 09:33:25 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Serve static files
+app.use(express.static("public"));
+
+// Home route
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
